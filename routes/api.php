85dd58b66c6d5c72485f65a5a5a5e97342f0c65f
pavelZem <pavelzem.rise@gmail.com>
2019-05-15 23:28:08 +0300

    created routes for users

--- routes/api.php
+++ routes/api.php
@@ -1,1 +1,10 @@
 <?php
+
+/***** Пользователи *****/
+
+//Получение
+Route::get('/users/{id}', 'UserController@get');
+//Регистрация
+Route::post('/sign-up', 'UserController@signUp');
+//Вход
+Route::post('/sign-in', 'UserController@signIn');
