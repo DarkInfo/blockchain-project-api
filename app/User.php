e68fae65019091e5f2bda96e50745dcfbe8adab9
pavelZem <pavelzem.rise@gmail.com>
2019-05-15 23:26:02 +0300

    created model for users

---
+++ app/User.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    //Таблица в бд
+    protected $table = 'users';
+    //Временные метки
+    public $timestamps = true;
+    //Массовое заполнение
+    protected $fillable = ['email', 'password', 'address'];
+    //Скрываемые поля при сериализации
+    protected $hidden = ['email','password', 'api_token'];
+
+}
