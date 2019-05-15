5533a82c65bdd1bf2f89284594ee55a5fe988cdd
pavelZem <pavelzem.rise@gmail.com>
2019-05-15 23:44:16 +0300

    created routes for contacts

--- routes/api.php
+++ routes/api.php
@@ -8,3 +8,16 @@ Route::get('/users/{id}', 'UserController@get');
 Route::post('/sign-up', 'UserController@signUp');
 //Вход
 Route::post('/sign-in', 'UserController@signIn');
+
+/***** КОНТАКТЫ *****/
+
+Route::group(['middleware' => 'auth:api'], function(){
+    //Создание контакта
+    Route::post('/contacts', 'ContactController@create');
+    //Обновление контакта
+    Route::put('/contacts', 'ContactController@update');
+    //Удаление контакта
+    Route::delete('/contacts', 'ContactController@delete');
+});
+//Получение контактов по id пользователя
+Route::get('/users/{user_id}/contacts', 'ContactController@get');
